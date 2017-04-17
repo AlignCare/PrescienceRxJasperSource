fbc0d2a9784909cd3dc5df5d18e4222729f84c00
steveattwell <steve.attwell@gmail.com>
2017-04-17 10:42:19 -0400

    case 35 added fields

--- MSSQL/openjson.sql
+++ MSSQL/openjson.sql
@@ -21,10 +21,20 @@ SELECT
 	[Age],
 	[DateOfBirthText] ,
 	[MonthYear],
+	[ConcurrentDrugsAllDrugs],
+	[COA],
+	[ZipCode],
+	[EmployerGroup],
+	[Product],
+	[Region],
+	[Group],
+	[Site],
+	[PCP],
 	[PredominentProviderNPI] ,
 	[PredominentProviderSpecialtyName],	
 	[PredominentProviderFullName],
-	[ConcurrentDrugsAllDrugs]
+	[IsACOParticipant],
+	[DifferentPrescribersInNetwork]
 FROM OPENJSON((SELECT[JSONREC]
   FROM [dbo].[rf0f1cdb2288d4e9b82eb307385b1aaed]))
   WITH (
@@ -48,9 +58,19 @@ FROM OPENJSON((SELECT[JSONREC]
 	[Age]   INT '$.Beneficiary.Age',
 	[DateOfBirthText] varchar(255) '$.Beneficiary.DateOfBirthText',
 	[MonthYear]   INT '$.Beneficiary.MonthYear',
+	[COA] varchar(255) '$.Beneficiary.COA',
+	[ZipCode] varchar(255) '$.Beneficiary.ZipCode',
+	[EmployerGroup] varchar(255) '$.Beneficiary.EmployerGroup',
+	[Product] varchar(255) '$.Beneficiary.Product',
+	[Region] varchar(255) '$.Beneficiary.Region',
+	[Group] varchar(255) '$.Beneficiary.Group',
+	[Site] varchar(255) '$.Beneficiary.Site',
+	[PCP] varchar(255) '$.Beneficiary.PCP',
 	[PredominentProviderNPI] varchar(255) '$.PredominentProvider.NPI',
 	[PredominentProviderSpecialtyName] varchar(255) '$.PredominentProvider.SpecialtyName',	
 	[PredominentProviderFullName] varchar(255) '$.PredominentProvider.FullName',
+	[IsACOParticipant] BIT '$.PredominentProvider.IsACOParticipant',
+
 	 structures nvarchar(max) AS JSON
 
   )
@@ -62,8 +82,17 @@ CROSS APPLY OPENJSON((SELECT[JSONREC]
   WITH (
   		[name] varchar(255) '$.name',
 		[ConcurrentDrugsAllDrugs] nvarchar(max) '$.data[0]' 
-  )
- WHERE name = 'All Drugs'
+  ) as ConcurrentDrugs
+
+CROSS APPLY OPENJSON((SELECT[JSONREC]
+  FROM [dbo].[rf0f1cdb2288d4e9b82eb307385b1aaed]),'$.DifferentPrescribers' ) 
+  WITH (
+  		[name] varchar(255) '$.name',
+		[DifferentPrescribersInNetwork] nvarchar(max) '$.data[0]' 
+  ) as DifferentPrescribers
+ WHERE 
+  ConcurrentDrugs.name = 'All Drugs' AND
+  DifferentPrescribers.name = 'In Network'
 
 
 
